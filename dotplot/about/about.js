0ce595266b08fc66623c9ebda0331680e5955514
draco <125047183+dotplotinseoul@users.noreply.github.com>
2023-03-26 03:10:31 +0900

    Add files via upload

---
+++ dotplot/about/about.js
@@ -0,0 +1,29 @@
+console.clear();
+
+$('.dot_second').click(function(){
+  $('.main_bar').toggleClass('showback');
+  $(this).toggleClass('big');
+});
+
+const $cursor = $('.cursor');
+
+$(window).mousemove(function(e) {
+  $cursor.css({
+    top:e.clientY,
+    left:e.clientX
+  });
+});
+
+$('.cursor-big').mouseenter(function(){
+    $('.cursor').addClass('need-to-cursor-big'),
+    $('.cursor').css('background-color', 'red')
+});
+
+$('.cursor-big').mouseleave(function(){
+    $('.cursor').removeClass('need-to-cursor-big')
+    $('.cursor').css('background-color', 'white')
+});
+
+window.onload = function(){
+  $('.loader').fadeOut();
+        };
